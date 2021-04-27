278a26b24a4a440f5db099eb94db8f670674cf5e
msc9533 <msc9533@gmail.com>
2021-04-27 20:44:47 +0900

    dataframe get row iter

--- house_price/main.py
+++ house_price/main.py
@@ -1,8 +1,19 @@
 import numpy as np
 import pandas as pd
+# trains
+"""Id,MSSubClass,MSZoning,LotFrontage,LotArea,Street,Alley,LotShape,LandContour,Utilities,LotConfig,LandSlope,Neighborhood,Condition1,Condition2,BldgType,HouseStyle,OverallQual,OverallCond,YearBuilt,YearRemodAdd,RoofStyle,RoofMatl,Exterior1st,Exterior2nd,MasVnrType,MasVnrArea,ExterQual,ExterCond,Foundation,BsmtQual,BsmtCond,BsmtExposure,BsmtFinType1,BsmtFinSF1,BsmtFinType2,BsmtFinSF2,BsmtUnfSF,TotalBsmtSF,Heating,HeatingQC,CentralAir,Electrical,1stFlrSF,2ndFlrSF,LowQualFinSF,GrLivArea,BsmtFullBath,BsmtHalfBath,FullBath,HalfBath,BedroomAbvGr,KitchenAbvGr,KitchenQual,TotRmsAbvGrd,Functional,Fireplaces,FireplaceQu,GarageType,GarageYrBlt,GarageFinish,GarageCars,GarageArea,GarageQual,GarageCond,PavedDrive,WoodDeckSF,OpenPorchSF,EnclosedPorch,3SsnPorch,ScreenPorch,PoolArea,PoolQC,Fence,MiscFeature,MiscVal,MoSold,YrSold,SaleType,SaleCondition"""
+
+# submission Id,SalePrice
+
+def train_data(filepath):
+    csv = pd.read_csv(filepath)
+    return csv
 
 def main():
-    pass
+    data = train_data('house_price/sample.csv')
+    print(data.keys())
+    for idx, row in data.iterrows():
+        print(idx,row)
 
 if __name__ == "__main__":
     main()
